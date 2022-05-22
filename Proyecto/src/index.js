a5d539a1805ea583bdaf033c3a18eff08310a817
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-05-22 13:50:00 +0200

    update

--- Proyecto/src/index.js
+++ Proyecto/src/index.js
@@ -18,6 +18,7 @@ app.use(express.json());
 
 app.use(require('./routes/index'));
 app.use('/api/pilotos', require('./routes/pilotos'));
+app.use('/api/pilotosNube', require('./routes/pilotosNube'));
 
 
 //inicia el servidor
