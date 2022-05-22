da8f729e34eb16f0d3ea8f12004da893388112b2
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-05-22 14:02:17 +0200

    Update pilotosNube.js

--- Proyecto/src/routes/pilotosNube.js
+++ Proyecto/src/routes/pilotosNube.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const fetch = require('node-fetch');
 router.get('/', async (req, res) => {
-    const response = await fetch('http://ergast.com/api/f1/constructors/mclaren/circuits/monza/drivers');
+    const response = await fetch('http://ergast.com/api/f1/drivers.JSON');
     const pilotos = await response.json();
     console.log(pilotos);
     res.send(pilotos);
