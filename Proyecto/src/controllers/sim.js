7a245b464a2c18dea85bfce4ff3303d03b417eac
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-05-27 18:44:01 +0200

    Add files via upload

--- Proyecto/src/controllers/sim.js
+++ Proyecto/src/controllers/sim.js
@@ -4,92 +4,161 @@
 /*let btn = document.getElementById('btn');
 btn.addEventListener('click', event => {
     almacenarDatos();
-});*/
+});*/  
 
-let dom = {};
-window.onload = () => {
-    dom['listado'] = document.getElementById('listado');
-    
 
-}
-function almacenarDatos() {
+function almacenarDatos(){
     let skill = document.getElementById('skill').value;
     console.log(skill);
     let exp = document.getElementById('exp').value;
     console.log(exp);
     let temporada = document.querySelector('#temporada').value
     console.log(temporada);
-
+    
     simular(skill, exp, temporada)
 }
 
-function simular(s, e, t) {
+function simular(s,e,t){
     /*Math.random() * (max - min) + min;*/
-    let puntosskill = s * 200 / 10
-    let puntosexp = e * 200 / 10
+    let puntosskill = s * 200 /10
+    let puntosexp = e * 200 /10
     let puntos = puntosskill + puntosexp
     console.log(puntos);
-    puntuar(puntos, t);
+    puntuar(puntos,t);
 }
 
-function puntuar(p, t) {
+function puntuar(p,t){
     let pos
     let carreras
-    if (t == "2020") {
+    let equipos
+    let pilotos
+    if (t == "2020"){
         p = p - 50
         carreras = 17
-        if (p > 347) {
-            pos = "La posicion simulada es: Primer puesto"
-        } else if (p > 223) {
-            pos = "La posicion simulada es: Segundo puesto"
-        } else if (p > 214) {
-            pos = "La posicion simulada es: Tercer puesto"
-        } else if (p > 125) {
-            pos = "La posicion simulada es: Cuarto puesto"
-        } else if (p > 119) {
-            pos = "La posicion simulada es: Quinto puesto"
-        } else if (p > 105) {
-            pos = "La posicion simulada es: Sexto puesto"
-        } else if (p > 98) {
-            pos = "La posicion simulada es: Octavo puesto"
-        } else if (p > 97) {
-            pos = "La posicion simulada es: Noveno puesto"
-        } else if (p > 75) {
-            pos = "La posicion simulada es: Decimo puesto"
-        } else if (p <= 75) {
+        equipos = [
+            "http://localhost:3000/api/equipos/1",
+            "http://localhost:3000/api/equipos/2",
+            "http://localhost:3000/api/equipos/3",
+            "http://localhost:3000/api/equipos/4",
+            "http://localhost:3000/api/equipos/5",
+            "http://localhost:3000/api/equipos/6",
+            "http://localhost:3000/api/equipos/7",
+            "http://localhost:3000/api/equipos/8",
+            "http://localhost:3000/api/equipos/9",
+            "http://localhost:3000/api/equipos/10"
+        ]
+        pilotos = [
+            "http://localhost:3000/api/pilotos/1",
+            "http://localhost:3000/api/pilotos/2",
+            "http://localhost:3000/api/pilotos/3",
+            "http://localhost:3000/api/pilotos/4",
+            "http://localhost:3000/api/pilotos/5",
+            "http://localhost:3000/api/pilotos/6",
+            "http://localhost:3000/api/pilotos/7",
+            "http://localhost:3000/api/pilotos/8",
+            "http://localhost:3000/api/pilotos/9",
+            "http://localhost:3000/api/pilotos/10",
+            "http://localhost:3000/api/pilotos/11",
+            "http://localhost:3000/api/pilotos/12",
+            "http://localhost:3000/api/pilotos/13",
+            "http://localhost:3000/api/pilotos/14",
+            "http://localhost:3000/api/pilotos/15",
+            "http://localhost:3000/api/pilotos/16",
+            "http://localhost:3000/api/pilotos/17",
+            "http://localhost:3000/api/pilotos/18",
+            "http://localhost:3000/api/pilotos/19",
+            "http://localhost:3000/api/pilotos/20",
+            "http://localhost:3000/api/pilotos/21",
+            "http://localhost:3000/api/pilotos/22",
+            "http://localhost:3000/api/pilotos/23"
+        ]
+        if (p > 347){
+            pos = "Tu posicion simulada es: Primer puesto"
+        }else if (p > 223){
+            pos = "Tu posicion simulada es: Segundo puesto"
+        }else if (p > 214){
+            pos = "Tu posicion simulada es: Tercer puesto"
+        }else if (p > 125){
+            pos = "Tu posicion simulada es: Cuarto puesto"
+        }else if (p > 119){
+            pos = "Tu posicion simulada es: Quinto puesto"
+        }else if (p > 105){
+            pos = "Tu posicion simulada es: Sexto puesto"
+        }else if (p > 98){
+            pos = "Tu posicion simulada es: Octavo puesto"
+        }else if (p > 97){
+            pos = "Tu posicion simulada es: Noveno puesto"
+        }else if (p > 75){
+            pos = "Tu posicion simulada es: Decimo puesto"
+        }else if (p <= 75){
             pos = "Estas fuera del top 10"
         }
-        post(pos, t, carreras)
-    } else if (t == "2021") {
+        post(pos,t,carreras,pilotos,equipos)
+    }else if (t == "2021"){
         carreras = 22
-        if (p > 395) {
-            pos = "La posicion simulada es: Primer puesto"
-        } else if (p > 387) {
-            pos = "La posicion simulada es: Segundo puesto"
-        } else if (p > 226) {
-            pos = "La posicion simulada es: Tercer puesto"
-        } else if (p > 190) {
-            pos = "La posicion simulada es: Cuarto puesto"
-        } else if (p > 165) {
-            pos = "La posicion simulada es: Quinto puesto"
-        } else if (p > 160) {
-            pos = "La posicion simulada es: Sexto puesto"
-        } else if (p > 159) {
-            pos = "La posicion simulada es: Septimo puesto"
-        } else if (p > 115) {
-            pos = "La posicion simulada es: Octavo puesto"
-        } else if (p > 110) {
-            pos = "La posicion simulada es: Noveno puesto"
-        } else if (p > 81) {
-            pos = "La posicion simulada es: Decimo puesto"
-        } else if (p <= 81) {
+        equipos = [
+            "http://localhost:3000/api/equipos/1",
+            "http://localhost:3000/api/equipos/2",
+            "http://localhost:3000/api/equipos/3",
+            "http://localhost:3000/api/equipos/4",
+            "http://localhost:3000/api/equipos/11",
+            "http://localhost:3000/api/equipos/6",
+            "http://localhost:3000/api/equipos/12",
+            "http://localhost:3000/api/equipos/8",
+            "http://localhost:3000/api/equipos/9",
+            "http://localhost:3000/api/equipos/10"
+        ]
+        pilotos = [
+            "http://localhost:3000/api/pilotos/1",
+            "http://localhost:3000/api/pilotos/2",
+            "http://localhost:3000/api/pilotos/3",
+            "http://localhost:3000/api/pilotos/4",
+            "http://localhost:3000/api/pilotos/5",
+            "http://localhost:3000/api/pilotos/6",
+            "http://localhost:3000/api/pilotos/8",
+            "http://localhost:3000/api/pilotos/9",
+            "http://localhost:3000/api/pilotos/10",
+            "http://localhost:3000/api/pilotos/11",
+            "http://localhost:3000/api/pilotos/12",
+            "http://localhost:3000/api/pilotos/13",
+            "http://localhost:3000/api/pilotos/16",
+            "http://localhost:3000/api/pilotos/17",
+            "http://localhost:3000/api/pilotos/20",
+            "http://localhost:3000/api/pilotos/23",
+            "http://localhost:3000/api/pilotos/24",
+            "http://localhost:3000/api/pilotos/25",
+            "http://localhost:3000/api/pilotos/26",
+            "http://localhost:3000/api/pilotos/27"
+        ]
+        if (p > 395){
+            pos = "Tu posicion simulada es: Primer puesto"
+        }else if (p > 387){
+            pos = "Tu posicion simulada es: Segundo puesto"
+        }else if (p > 226){
+            pos = "Tu posicion simulada es: Tercer puesto"
+        }else if (p > 190){
+            pos = "Tu posicion simulada es: Cuarto puesto"
+        }else if (p > 165){
+            pos = "Tu posicion simulada es: Quinto puesto"
+        }else if (p > 160){
+            pos = "Tu posicion simulada es: Sexto puesto"
+        }else if (p > 159){
+            pos = "Tu posicion simulada es: Septimo puesto"
+        }else if (p > 115){
+            pos = "Tu posicion simulada es: Octavo puesto"
+        }else if (p > 110){
+            pos = "Tu posicion simulada es: Noveno puesto"
+        }else if (p > 81){
+            pos = "Tu posicion simulada es: Decimo puesto"
+        }else if (p <= 81){
             pos = "Estas fuera del top 10"
         }
-        post(pos, t, carreras)
+    post(pos,t,carreras,pilotos,equipos)
     }
     console.log(pos);
+    
 }
-function post(p, t, n) {
+function post(p,t,n,pilotos,e){
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:3000/api/temporadas/", false);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -98,8 +167,10 @@ function post(p, t, n) {
         "edicion": "Edicion simulada ",
         "tupos": p,
         "resumen": "",
+        "pilotos": pilotos,
         "carrerasDisputadas": "" + n,
-
+        "equipos": e,
+        
     }));
 }
 
